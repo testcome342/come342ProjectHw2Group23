0fbec1ac52e45dfa7c34a6aa0b9f7c69f6bcbad7
Sumeyyearsllan <36950988+Sumeyyearsllan@users.noreply.github.com>
2018-04-17 13:31:11 +0300

    assignStaff
    homework2

---
+++ homework2a.java
@@ -0,0 +1,11 @@
+package homework2;
+//tugbaayvaz 201531075
+public interface AssignStaffUI {
+	
+	
+	public void startInterface();
+	public void selectStaff();
+	public void createCampaignTeam();
+	
+	
+}
